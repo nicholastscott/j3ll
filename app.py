1d212f5222926ff5d836f09e706460ba815dee81
Nick <11700037+nicholastscott@users.noreply.github.com>
2024-09-09 20:30:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 with st.sidebar:
     st.logo("logos/logo-no-background.png")
     st.title("Built to classify 311 data")
-    choice = st.radio("Navigation", ["","",])
+    choice = st.radio("Agency", ["","",])
 st.write("Hello!")
