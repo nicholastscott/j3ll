bdc7242314f23a7b4b82c34c1f148673ca1bade9
Nick <11700037+nicholastscott@users.noreply.github.com>
2024-09-09 21:42:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,11 @@ import streamlit as st
 with st.sidebar:
     st.logo("logos/logo-no-background.png")
     st.title("Built to classify 311 data")
-    choice = st.radio("Agency", ["","",])
+    choice = st.radio("Agency", ["All","Streets Department","Philly311 Contact Center",])
+    st.info("This application was designed with the citizen and call center responder in mind. It allows while suggesting: 1. Agency Assignment and 2. Importance/Urgency")
+
+
 st.write("Hello!")
+
+if choice == "":
+    pass
